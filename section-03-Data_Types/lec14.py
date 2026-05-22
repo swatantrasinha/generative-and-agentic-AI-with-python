9a01a84e087bf97463ddfe70be7a71497a8a2710
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2026-05-22 08:42:58 +0530

    initial commit

---
+++ section-03-Data_Types/lec14.py
@@ -0,0 +1,9 @@
+sugar_amount=2
+print(f"Initial Sugar Amount: {sugar_amount}")
+print(f"ID of 2: {id(2)}")
+
+print("------------------")
+
+sugar_amount=12
+print(f"New Sugar Amount: {sugar_amount}")
+print(f"ID of 12: {id(12)}")
