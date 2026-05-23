3f73d0976dc935b3501f9c06fda72915e3fd43b1
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2026-05-24 01:36:07 +0530

    mutable and immutable onj

--- section-03-Data_Types/lec14.py
+++ section-03-Data_Types/lec14.py
@@ -1,18 +1,18 @@
-# sugar_amount=2
-# print(f"Initial Sugar Amount: {sugar_amount}")
-# print(f"ID of 2: {id(2)}")
-# print(f"ID of sugar_amount: {id(sugar_amount)}")
+sugar_amount=2
+print(f"Initial Sugar Amount: {sugar_amount}")
+print(f"ID of 2: {id(2)}")
+print(f"ID of sugar_amount: {id(sugar_amount)}")
 
-# print("------------------")
+print("------------------")
 
-# sugar_amount=12
-# print(f"New Sugar Amount: {sugar_amount}")
-# print(f"ID of 12: {id(12)}")
-# print(f"ID of sugar_amount: {id(sugar_amount)}")
+sugar_amount=12
+print(f"New Sugar Amount: {sugar_amount}")
+print(f"ID of 12: {id(12)}")
+print(f"ID of sugar_amount: {id(sugar_amount)}")
 
-# print("=================================")
-# print("*********************************")
-# print("=================================")
+print("=================================")
+print("*********************************")
+print("=================================")
 
 spce_mix= set()
 print("Initial : spice_mix is: ", spce_mix)
