110ad0cadbec01c09832f62d2069ffa132391492
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2026-05-24 01:35:12 +0530

    initial commit

--- section-03-Data_Types/lec14.py
+++ section-03-Data_Types/lec14.py
@@ -1,9 +1,24 @@
-sugar_amount=2
-print(f"Initial Sugar Amount: {sugar_amount}")
-print(f"ID of 2: {id(2)}")
+# sugar_amount=2
+# print(f"Initial Sugar Amount: {sugar_amount}")
+# print(f"ID of 2: {id(2)}")
+# print(f"ID of sugar_amount: {id(sugar_amount)}")
 
-print("------------------")
+# print("------------------")
 
-sugar_amount=12
-print(f"New Sugar Amount: {sugar_amount}")
-print(f"ID of 12: {id(12)}")
+# sugar_amount=12
+# print(f"New Sugar Amount: {sugar_amount}")
+# print(f"ID of 12: {id(12)}")
+# print(f"ID of sugar_amount: {id(sugar_amount)}")
+
+# print("=================================")
+# print("*********************************")
+# print("=================================")
+
+spce_mix= set()
+print("Initial : spice_mix is: ", spce_mix)
+print(f"Initial spce_mix id : {id(spce_mix)}")
+print("------")
+spce_mix.add("ginger")
+spce_mix.add("cardamon")
+print("After: spice_mix is: ", spce_mix)
+print(f"After spce_mix id : {id(spce_mix)}")
